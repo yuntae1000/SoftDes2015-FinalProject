67f648fc6be1c28cde57ac6c21356bfc2ad27def
YoungHwa <yhkimmay@gmail.com>
2015-11-17 00:33:34 -0500

    soeun

--- try3.py
+++ try3.py
@@ -21,7 +21,6 @@ class parse:
 				recipe.append(y[0])
 		return recipe
 
-
 	def closetcooking(self, url):
 		recipe = []
 		html = urllib2.urlopen(url).read()
@@ -36,42 +35,54 @@ class parse:
 					recipe.append(d)
 		return recipe
 		
-	def	recipe(self, url):
-		if self.url[11] == 'e':
-			u = urllib2.urlopen(self.url)
-			u = u.read()
-			u = plaintext(u)
-
-			recipe = u.split('\n')
-
-
-
-			cutting_num1 = recipe.index('Preparation')+3
-			cutting_num2 = recipe.index('Exclusive Offers') - 1
-			recipe = recipe[cutting_num1:cutting_num2]
-		if self.url[11] == 'b':
-			u = urllib2.urlopen(self.url)
-			u = u.read()
-			u = plaintext(u)
-
-			recipe = u.split('\n')
-			num = 0
-			while num<len(recipe):
-				text = recipe[num]
-				recipe[num] = text.encode('ascii','ignore')
-				num = num + 1
-
-			cutting_num1 = recipe.index('Preparation')+3
-			cutting_num2 = recipe.index('See More') - 6
-			recipe = recipe[cutting_num1:cutting_num2]
-			for i in range(len(recipe)):
-				if recipe[i+1] == '' or '*':
-					del recipe[i+1]
-
+	def epicurious(self, url):
+		u = urllib2.urlopen(self.url)
+		u = u.read()
+		u = plaintext(u)
+		recipe = u.split('\n')
+		num = 0
+		while num<len(recipe):
+			text = recipe[num]
+			recipe[num] = text.encode('ascii','ignore')
+			num = num + 1
+		cutting_num1 = recipe.index('Preparation')+3
+		cutting_num2 = recipe.index('Exclusive Offers') - 1
+		recipe = recipe[cutting_num1:cutting_num2]
+		recipe = [x for x in recipe if x != '']
+		recipe = [x for x in recipe if x != '*']
 		return recipe
-	def ingredients(self, url):
-		url2 = urllib2.urlopen(self.url)
-		
 
-in_put = parse('http://www.bonappetit.com/recipe/pan-roasted-chicken-with-pineapple-chile-glaze')
-print in_put.recipe('http://www.bonappetit.com/recipe/pan-roasted-chicken-with-pineapple-chile-glaze')
+	def bonappetit(self, url):
+		u = urllib2.urlopen(self.url)
+		u = u.read()
+		u = plaintext(u)
+		recipe = u.split('\n')
+		num = 0
+		while num<len(recipe):
+			text = recipe[num]
+			recipe[num] = text.encode('ascii','ignore')
+			num = num + 1
+		cutting_num1 = recipe.index('Preparation')+3
+		cutting_num2 = recipe.index('See More') - 6
+		recipe = recipe[cutting_num1:cutting_num2]
+		recipe = [x for x in recipe if x != '']
+		recipe = [x for x in recipe if x != '*']
+		return recipe
+
+	def	recipe(self, url):
+		if self.url[11] == 'a':
+			parse.allrecipes(url)
+		if self.url[11] == 'c':	
+			parse.closetcooking(url)
+		if self.url[11] == 'e':
+			parse.epicurious(url)
+		if self.url[11] == 'b':
+			parse.bonappetit(url)
+		
+url = 'http://www.bonappetit.com/recipe/pan-roasted-chicken-with-pineapple-chile-glaze'
+parse = parse(url)
+#in_put = parse('http://www.bonappetit.com/recipe/pan-roasted-chicken-with-pineapple-chile-glaze')
+print parse.recipe(url)
+
+#in_put = 'http://www.bonappetit.com/recipe/pan-roasted-chicken-with-pineapple-chile-glaze'
+#print parse.recipe('http://www.bonappetit.com/recipe/pan-roasted-chicken-with-pineapple-chile-glaze')		
