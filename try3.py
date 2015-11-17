72d54a058457f6ac9ac59e49740bfdb4121a2dad
soeun kim <kimsoun0107@kaist.ac.kr>
2015-11-16 23:53:20 -0500

    for y

--- try3.py
+++ try3.py
@@ -4,6 +4,37 @@ from pattern.web import *
 class parse:
 	def __init__(self, url):
 		self.url = url
+
+	def allrecipes(self, url):
+		recipe = []
+		html = urllib2.urlopen(url).read()
+		a = html.split("""<li class="step" ng-class="{'finished': stepIsActive0}" ng-click="stepIsActive0 = !stepIsActive0"><span class="recipe-directions__list--item">""")
+		b = a[1].split("""<ol class="list-numbers recipe-directions__list recipeNotes ng-hide" ng-show="itemNote" ng-cloak>""")
+		c = b[0].split("""<li class="step" ng-class="{'finished': stepIsActive""")
+		for i in range(len(c)):
+			if i==0 :
+				x = c[0].split('<')
+				recipe.append(x[0])
+			else :
+				x = c[i].split('>')
+				y = x[2].split('<')
+				recipe.append(y[0])
+		return recipe
+
+
+	def closetcooking(self, url):
+		recipe = []
+		html = urllib2.urlopen(url).read()
+		text = plaintext(html)
+		a = text.split('directions')
+		b = a[1].split('\n\n')
+		c = b[1].split('\n')
+		for i in range(len(c)):
+			if len(c[i]) > 0:
+				if c[i][0]=='*':
+					d = c[i][2:]
+					recipe.append(d)
+		return recipe
 		
 	def	recipe(self, url):
 		if self.url[11] == 'e':
@@ -12,11 +43,7 @@ class parse:
 			u = plaintext(u)
 
 			recipe = u.split('\n')
-			num = 0
-			while num<len(recipe):
-				text = recipe[num]
-				recipe[num] = text.encode('ascii','ignore')
-				num = num + 1
+
 
 
 			cutting_num1 = recipe.index('Preparation')+3
