826dce3bd02cb63c468d857b19f1ecb31bbb5380
Jong Woo Nam <jwnam0709@gmail.com>
2015-12-13 02:38:37 -0500

    signup working

--- cooksmart_flask/FlaskApp/__init__.py
+++ cooksmart_flask/FlaskApp/__init__.py
@@ -1,16 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
 
+
+mysql = MySQL()
 app = Flask(__name__)
 
-@app.route('/')
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'WhddkfWhddkf92'
+app.config['MYSQL_DATABASE_DB'] = 'cooksmart'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route('/',methods=['POST','GET'])
 def homepage():
+
     return render_template("main.html")
+
 
 @app.route('/dashboard/')
 def dashboard():
     return render_template("example.html")
 
+@app.route('/register',methods=['POST','GET'])
+def signUp():
+    try:
+        _name = request.form['username']
+        _email = request.form['email']
+        _password = request.form['password']
 
+        # validate the received values
+        if _name and _email and _password:
+            
+            # All Good, let's call MySQL
+            
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
